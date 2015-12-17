e36538e84479bfbcddef51f37cc7ea508e6d82c6
Jordi Prats <jprats@systemadmin.es>
2015-12-17 17:41:12 +0100

    copy-paste, never again

--- lib/facter/eyp_tomcat_check_java.rb
+++ lib/facter/eyp_tomcat_check_java.rb
@@ -3,7 +3,7 @@ java=Facter::Util::Resolution.exec('bash -c \'which java 2>/dev/null \'')
 
 Facter.add('eyp_tomcat_check_java') do
     setcode do
-      if (env.nil? or env.empty?)
+      if (java.nil? or java.empty?)
         false
       else
         true
