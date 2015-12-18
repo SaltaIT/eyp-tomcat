9963ad93a1180f417de7865615316c6fc5a3b872
Jordi Prats <jprats@systemadmin.es>
2015-12-18 12:17:12 +0100

    netejar merda

--- lib/facter/eyp_tomcat_check_jdk.rb
+++ lib/facter/eyp_tomcat_check_jdk.rb
@@ -1,6 +1,6 @@
 # file $(dirname $(dirname $(dirname $(find / -xdev -iname jni_md.h | head -n1))))/bin/java >/dev/null 2>&1; echo $?
 
-jdk=Facter::Util::Resolution.exec('bash -c \'file $(dirname $(dirname $(dirname $(find / -xdev -iname jni_md.h | head -n1))))/bin/java >/dev/null 2>&1; echo $?\'')
+jdk=Facter::Util::Resolution.exec('bash -c \'file $(dirname $(dirname $(dirname $(find / -xdev -iname jni_md.h | head -n1))))/bin/java >/dev/null 2>&1; echo $?\' 2>/dev/null')
 
 Facter.add('eyp_tomcat_check_jdk') do
     setcode do
