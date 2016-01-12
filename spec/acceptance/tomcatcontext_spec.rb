807aeb57334cfbb53a0eb3b56afe824dca4c9230
Jordi Prats <jprats@systemadmin.es>
2016-01-12 11:22:08 +0100

    error contet

--- spec/acceptance/tomcatcontext_spec.rb
+++ spec/acceptance/tomcatcontext_spec.rb
@@ -27,17 +27,18 @@ describe 'tomcat context' do
       # Run it twice and test for idempotency
       expect(apply_manifest(pp).exit_code).to_not eq(1)
       expect(apply_manifest(pp).exit_code).to eq(0)
-
-      # File.read("/opt/tomcat-8080/conf/context.xml").expect include "sessionCookiePath=\"/\""
-      #expect(File.read("/opt/tomcat-8080/conf/context.xml")).to match("sessionCookiePath=\"/\"")
-      it { should contain_file('/opt/tomcat-8080/conf/context.xml').with({
-        'content' => /sessionCookiePath="\/"/})
-      }
-
-      # File.read("/opt/tomcat-8080/conf/context.xml").should include "antiJARLocking=\"true\""
-      # File.read("/opt/tomcat-8080/conf/context.xml").should include "antiResourceLocking=\"true\""
-      # File.read("/opt/tomcat-8080/conf/context.xml").should include "<WatchedResource>WEB-INF/web.xml</WatchedResource>"
-      # File.read("/opt/tomcat-8080/conf/context.xml").should include "<Manager pathname=\"\" />"
     end
+
+    # File.read("/opt/tomcat-8080/conf/context.xml").expect include "sessionCookiePath=\"/\""
+    #expect(File.read("/opt/tomcat-8080/conf/context.xml")).to match("sessionCookiePath=\"/\"")
+    it { should contain_file('/opt/tomcat-8080/conf/context.xml').with({
+      'content' => /sessionCookiePath="\/"/})
+    }
+
+    # File.read("/opt/tomcat-8080/conf/context.xml").should include "antiJARLocking=\"true\""
+    # File.read("/opt/tomcat-8080/conf/context.xml").should include "antiResourceLocking=\"true\""
+    # File.read("/opt/tomcat-8080/conf/context.xml").should include "<WatchedResource>WEB-INF/web.xml</WatchedResource>"
+    # File.read("/opt/tomcat-8080/conf/context.xml").should include "<Manager pathname=\"\" />"
+
   end
 end
