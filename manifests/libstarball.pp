cc4bd668a7d07d176488bc7120b3ba106bdcd7c8
Jordi Prats <jprats@systemadmin.es>
2016-02-09 08:44:47 +0100

    afegit purgat libs

--- manifests/libstarball.pp
+++ manifests/libstarball.pp
@@ -3,6 +3,7 @@ define tomcat::libstarball (
                             $source,
                             $servicename=$name,
                             $libstarballname=$name,
+                            $purge_old=true,
                           ) {
 
   if ! defined(Class['tomcat'])
@@ -19,21 +20,39 @@ define tomcat::libstarball (
     $serviceinstance=Service[$servicename]
   }
 
+  if($purge_old)
+  {
+    $notify_updated_tarball=Exec[ [ "cleanup ${libstarballname} ${catalina_base}",
+                                    "tar xzf ${libstarballname} ${catalina_base}" ] ]
+
+    exec { "cleanup ${libstarballname} ${catalina_base}":
+      command     => "rm -fr ${catalina_base}/lib/* ${catalina_base}/lib/.*",
+      refreshonly => true,
+      notify      => Exec["tar xzf ${libstarballname} ${catalina_base}"],
+      require     => File["${catalina_base}/.libs.${libstarballname}.tgz"],
+      before      => Exec["tar xzf ${libstarballname} ${catalina_base}"],
+    }
+  }
+  else
+  {
+    $notify_updated_tarball=Exec["tar xzf ${libstarballname} ${catalina_base}"]
+  }
+
   file { "${catalina_base}/.libs.${libstarballname}.tgz":
     ensure  => 'present',
     owner   => 'root',
     group   => 'root',
     mode    => '0400',
     require => [File[$catalina_base], File["${catalina_base}/lib"]],
-    notify  => Exec["tar xzf ${libstarballname} ${catalina_base}"],
+    notify  => $notify_updated_tarball,
     source  => $source,
   }
 
   exec { "tar xzf ${libstarballname} ${catalina_base}":
-    command => "tar --no-same-owner --strip 1 -xzf ${catalina_base}/.libs.${libstarballname}.tgz -C ${catalina_base}/lib",
+    command     => "tar --no-same-owner --strip 1 -xzf ${catalina_base}/.libs.${libstarballname}.tgz -C ${catalina_base}/lib",
     refreshonly => true,
-    notify  => $serviceinstance,
-    require => File["${catalina_base}/.libs.${libstarballname}.tgz"],
+    notify      => $serviceinstance,
+    require     => File["${catalina_base}/.libs.${libstarballname}.tgz"],
   }
 
 }
