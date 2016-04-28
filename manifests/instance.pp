eb313a122de19c2fb91afd7b917d779431a67d12
Jordi Prats <jprats@systemadmin.es>
2016-04-28 17:59:55 +0200

    oops xD

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -372,14 +372,14 @@ define tomcat::instance (
 
   if($install_tomcat_manager)
   {
-    exec { 'cp tomcat manager from tomcat-home':
+    exec { "cp tomcat manager from tomcat-home ${instancename}":
       command => "cp -pr ${tomcat::catalina_home}/webapps/manager ${catalina_base}/webapps",
       creates => "${catalina_base}/webapps/manager",
       require => File["${catalina_base}/webapps"],
       before => Service[$instancename],
     }
 
-    exec { 'cp tomcat host-manager from tomcat-home':
+    exec { "cp tomcat host-manager from tomcat-home ${instancename}":
       command => "cp -pr ${tomcat::catalina_home}/webapps/host-manager ${catalina_base}/webapps",
       creates => "${catalina_base}/webapps/host-manager",
       require => File["${catalina_base}/webapps"],
