1a5c1ab3e0e320b27cdd1b4642efa851bbf13d00
Jordi Prats <jprats@systemadmin.es>
2016-05-06 19:55:48 +0200

    detall ports

--- spec/acceptance/tomcat_6666_standard_spec.rb
+++ spec/acceptance/tomcat_6666_standard_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper_acceptance'
 
 describe 'tomcat class' do
 
-
-
   context 'basic setup (URL) - explicitly testing native library' do
 
     it "kill java" do
@@ -20,34 +18,34 @@ describe 'tomcat class' do
 
       class { 'tomcat':
     		tomcat_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
-        nativelibrary => true,
+        nativelibrary => false,
     	}
 
-    	tomcat::instance { 'tomcat-8080':
+    	tomcat::instance { 'tomcat-6666':
         tomcatpw => 'lol',
-        shutdown_port=>'2022',
-        ajp_port=>'8081',
-        connector_port=>'8080',
-        jmx_port => '2021',
+        shutdown_port=>'6661',
+        ajp_port=>'6662',
+        connector_port=>'6666',
+        jmx_port => '6663',
         lockoutrealm => false,
         java_library_path => '/usr/local/apr/lib/:/usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib',
     	}
 
-      tomcat::instance { 'tomcat-8888':
+      tomcat::instance { 'tomcat-6668':
         tomcatpw => 'lol',
-        shutdown_port=>'9000',
-        ajp_port=>'8885',
-        connector_port=>'8888',
-        jmx_port => '9999',
+        shutdown_port=>'6669',
+        ajp_port=>'6667',
+        connector_port=>'6668',
+        jmx_port => '6665',
         lockoutrealm => true,
         java_library_path => '/usr/local/apr/lib/:/usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib',
       }
 
-      tomcat::driver::postgres { 'tomcat-8080':
+      tomcat::driver::postgres { 'tomcat-6666':
         ensure => 'absent',
       }
 
-      tomcat::driver::postgres { 'tomcat-8888':
+      tomcat::driver::postgres { 'tomcat-6668':
         ensure => 'absent',
       }
 
@@ -76,74 +74,74 @@ describe 'tomcat class' do
     end
 
     #/var/run/
-    it "PID tomcat-8080" do
-      expect(shell("cat /var/run/tomcat-8080.pid").exit_code).to be_zero
+    it "PID tomcat-6666" do
+      expect(shell("cat /var/run/tomcat-6666.pid").exit_code).to be_zero
     end
 
-    it "PID tomcat-8888" do
-      expect(shell("cat /var/run/tomcat-8888.pid").exit_code).to be_zero
+    it "PID tomcat-6668" do
+      expect(shell("cat /var/run/tomcat-6668.pid").exit_code).to be_zero
     end
 
     #/opt/tomcat-8080/webapps/host-manager
-    it "host-manager tomcat 8080" do
-      expect(shell("ls -la /opt/tomcat-8080/webapps/host-manager").exit_code).to be_zero
+    it "host-manager tomcat 6666" do
+      expect(shell("ls -la /opt/tomcat-6666/webapps/host-manager").exit_code).to be_zero
     end
 
 
     #/opt/tomcat-8080/logs/catalina.out
-    it "catalina log 8080" do
-      expect(shell("cat /opt/tomcat-8080/logs/catalina.out").exit_code).to be_zero
+    it "catalina log 6666" do
+      expect(shell("cat /opt/tomcat-6666/logs/catalina.out").exit_code).to be_zero
     end
 
-    it "catalina log 8888" do
-      expect(shell("cat /opt/tomcat-8888/logs/catalina.out").exit_code).to be_zero
+    it "catalina log 6668" do
+      expect(shell("cat /opt/tomcat-6668/logs/catalina.out").exit_code).to be_zero
     end
 
     #HTTP connector
 
-    #instance tomcat-8080
-    describe port(8085) do
+    #instance tomcat-6666
+    describe port(6666) do
       it { should be_listening }
     end
 
-    #instance tomcat-8888 HTTP connector
-    describe port(8889) do
+    #instance tomcat-6668 HTTP connector
+    describe port(6668) do
       it { should be_listening }
     end
 
     #shutdown port
 
-    #instance tomcat-8080
-    describe port(2022) do
+    #instance tomcat-6666
+    describe port(6661) do
       it { should be_listening }
     end
 
-    #instance tomcat-8888
-    describe port(9000) do
+    #instance tomcat-6668
+    describe port(6669) do
       it { should be_listening }
     end
 
     #JMX port
 
-    #instance tomcat-8080
-    describe port(2021) do
+    #instance tomcat-6666
+    describe port(6663) do
       it { should be_listening }
     end
 
-    #instance tomcat-8888
-    describe port(9999) do
+    #instance tomcat-6668
+    describe port(6665) do
       it { should be_listening }
     end
 
     #AJP port
 
-    #instance tomcat-8080
-    describe port(8081) do
+    #instance tomcat-6666
+    describe port(6662) do
       it { should be_listening }
     end
 
-    #instance tomcat-8888
-    describe port(8885) do
+    #instance tomcat-6668
+    describe port(6667) do
       it { should be_listening }
     end
 
@@ -151,12 +149,12 @@ describe 'tomcat class' do
     ### tomcat-users.xml
     # tomcat admin password
     # <user username="tomcat" password="403926033d001b5279df37cbbe5287b7c7c267fa"
-    describe file("/opt/tomcat-8080/conf/tomcat-users.xml") do
+    describe file("/opt/tomcat-6666/conf/tomcat-users.xml") do
       it { should be_file }
       its(:content) { should match '<user username="tomcat" password="403926033d001b5279df37cbbe5287b7c7c267fa"' }
     end
 
-    describe file("/opt/tomcat-8888/conf/tomcat-users.xml") do
+    describe file("/opt/tomcat-6668/conf/tomcat-users.xml") do
       it { should be_file }
       its(:content) { should match '<user username="tomcat" password="403926033d001b5279df37cbbe5287b7c7c267fa"' }
     end
@@ -164,13 +162,13 @@ describe 'tomcat class' do
     ### server.xml
     #org.apache.catalina.realm.UserDatabaseRealm
 
-    describe file("/opt/tomcat-8080/conf/server.xml") do
+    describe file("/opt/tomcat-6666/conf/server.xml") do
       it { should be_file }
       its(:content) { should match 'digest="sha"' }
       its(:content) { should_not match 'LockOutRealm' }
     end
 
-    describe file("/opt/tomcat-8888/conf/server.xml") do
+    describe file("/opt/tomcat-6668/conf/server.xml") do
       it { should be_file }
       its(:content) { should match 'org.apache.catalina.realm.UserDatabaseRealm' }
       #defaul sha
@@ -182,13 +180,12 @@ describe 'tomcat class' do
     end
 
     #configtest server.xml
-    #CATALINA_BASE=/opt/tomcat-8080 /opt/tomcat-home/bin/catalina.sh configtest
-    it 'configtest server.xml tomcat-8080' do
-      expect(shell("CATALINA_BASE=/opt/tomcat-8080 /opt/tomcat-home/bin/catalina.sh configtest >/dev/null 2>&1").exit_code).to be_zero
+    it 'configtest server.xml tomcat-6666' do
+      expect(shell("CATALINA_BASE=/opt/tomcat-6666 /opt/tomcat-home/bin/catalina.sh configtest >/dev/null 2>&1").exit_code).to be_zero
     end
 
-    it 'configtest server.xml tomcat-8888' do
-      expect(shell("CATALINA_BASE=/opt/tomcat-8888 /opt/tomcat-home/bin/catalina.sh configtest >/dev/null 2>&1").exit_code).to be_zero
+    it 'configtest server.xml tomcat-6668' do
+      expect(shell("CATALINA_BASE=/opt/tomcat-6668 /opt/tomcat-home/bin/catalina.sh configtest >/dev/null 2>&1").exit_code).to be_zero
     end
 
   end
