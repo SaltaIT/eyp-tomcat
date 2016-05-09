245bdfbcb4dd2e66e26dbc0050ff13455f411e76
Jordi Prats <jprats@systemadmin.es>
2016-05-09 16:25:07 +0200

    canvi marranada per un jrunscript

--- spec/acceptance/TODO_tomcat_5555_native_library_TODO.rb
+++
@@ -1,5 +0,0 @@
-require 'spec_helper_acceptance'
-
-# tomcat-5555
-
-#TODO: testing native library
---
+++ spec/acceptance/tomcat_5555_native_library_spec.rb
@@ -0,0 +1,48 @@
+require 'spec_helper_acceptance'
+
+# tomcat-5555
+
+#TODO: testing native library
+
+describe 'tomcat class' do
+
+  context 'simple port testing' do
+    # Using puppet_apply as a helper
+
+    it "kill java" do
+      expect(shell("bash -c 'pkill java; for i in $(netstat -tpln | grep java | rev | grep -Eo /[0-9]* | rev | cut -f1 -d/); do kill $i; sleep 10; kill -9 $i; sleep 10; done'").exit_code).to be_zero
+    end
+
+    it "fuck tomcats" do
+      expect(shell("bash -c 'rm -fr /usr/local/src/tomcat* /opt/tomcat* /etc/init.d/tomcat*; sleep 5'").exit_code).to be_zero
+    end
+
+
+    it 'should work with no errors' do
+      pp = <<-EOF
+
+      class { 'tomcat':
+        tomcat_url => 'http://archive.apache.org/dist/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz',
+        nativelibrary => true,
+      }
+
+      tomcat::instance { 'tomcat-5555':
+        tomcatpw => 'lol',
+        shutdown_port=>'5553',
+        ajp_port=>'5552',
+        connector_port=>'5555',
+        jmx_port => '5554',
+        java_library_path => '/usr/local/apr/lib/:/usr/java/packages/lib/amd64:/usr/lib64:/lib64:/lib:/usr/lib',
+    	}
+
+      EOF
+
+      # Run it twice and test for idempotency
+      expect(apply_manifest(pp).exit_code).to_not eq(1)
+      expect(apply_manifest(pp).exit_code).to eq(0)
+
+    end
+
+  end
+
+end
