c3275639af2baba5d981ffc77b94b1b56ec2b329
Jordi Prats <jprats@systemadmin.es>
2016-10-22 17:54:36 +0200

    documentació JNDI

--- manifests/resource.pp
+++ manifests/resource.pp
@@ -1,3 +1,24 @@
+# == ORACLE JDBC ==
+#
+# The relevant documentation for this is quite heavy going, but these are key documents
+#
+#	http://tomcat.apache.org/tomcat-6.0-doc/jndi-datasource-examples-howto.html
+#	http://docs.oracle.com/cd/B28359_01/java.111/b31224/concache.htm
+#	https://forums.oracle.com/forums/thread.jspa?threadID=967849
+#
+# To set debug logging see following URL and edit logging.properties
+#
+#	http://dev-answers.blogspot.com/2010/03/enable-debugtrace-level-logging-for.html
+#
+# To reference include files from server.xml see
+#
+#	http://blogs.mulesoft.org/including-files-into-tomcats-server-xml-using-xml-entity-includes/
+#
+# Note this subtle but crucial difference for SID vs SERVICE_NAME based connections.
+#
+#	Old form: jdbc:oracle:thin:<host>:<port>:<SID>
+#	New form: jdbc:oracle:thin:@<host>:<port>/<SERVICE_NAME>
+#
 define tomcat::resource (
                           $resource_type,
                           $resource_name,
