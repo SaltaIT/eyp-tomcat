d410f75fe200bd04de1e4f2854d53eae87049b16
Jordi Prats <jprats@systemadmin.es>
2016-10-26 17:31:23 +0200

    detecció contenidor

--- manifests/instance/service.pp
+++ manifests/instance/service.pp
@@ -6,6 +6,9 @@ define tomcat::instance::service(
                                   $instancename          = $name,
                                 ) {
   #
+  $is_docker_container_var=getvar('::eyp_docker_iscontainer')
+  $is_docker_container=str2bool($is_docker_container_var)
+
   if( $is_docker_container==false or
       $manage_docker_service)
   {
