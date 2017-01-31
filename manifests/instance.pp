aeac27800f604dfc6f25cbf62f8c56aa1764a351
Jordi Prats <jprats@systemadmin.es>
2017-01-31 12:55:08 +0100

    bugfix dependencia

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -447,7 +447,6 @@ define tomcat::instance (
     group   => 'root',
     mode    => '0755',
     require => [ File[ [
-                        "${catalina_base}/conf/tomcat-users.xml",
                         "${catalina_base}/lib",
                         "${catalina_base}/logs",
                         "${catalina_base}/temp",
@@ -460,6 +459,7 @@ define tomcat::instance (
                         "${catalina_base}/bin/configtest.sh",
                       ] ],
                   Concat[ [ "${catalina_base}/bin/setenv.sh",
+                            "${catalina_base}/conf/tomcat-users.xml",
                             "${catalina_base}/conf/server.xml" ] ] ],
     content => template("${module_name}/multi/tomcat-init.erb"),
     notify  => Tomcat::Instance::Service[$instancename],
