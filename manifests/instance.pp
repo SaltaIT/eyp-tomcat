47aa54f26033cf5415256838b226c3bd83585c7a
Jordi Prats <jprats@systemadmin.es>
2017-05-09 14:27:25 +0200

    arrow on right

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -104,7 +104,9 @@ define tomcat::instance (
                           $disable_explicit_gc                   = false,
                           $max_gc_pause_millis                   = undef,
                           $print_gc                              = false,
+                          $print_gc_details                      = false,
                           $print_gc_datestamps                   = false,
+                          $print_gc_application_stopped_time     = false,
                         ) {
   Exec {
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
