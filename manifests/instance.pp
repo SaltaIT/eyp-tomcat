6bebccddf0f07267d0082d8fd743994857da9300
Jordi Prats <jprats@systemadmin.es>
2017-05-29 12:39:18 +0200

    coses del march

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -41,6 +41,7 @@ define tomcat::instance (
                           $shutdown_address                      = '127.0.0.1',
                           $ajp_port                              = undef,
                           $connector_ajp_packet_size             = undef,
+                          $connector_ajp_max_threads             = undef,
                           $connector_port                        = '8080',
                           $connector_http_enabled                = true,
                           $connector_http_server                 = undef,
@@ -130,9 +131,8 @@ define tomcat::instance (
                           $java_awt_headless                     = true,
                           $java_security_egd                     = undef,
                           $server                                = false,
-                          $startStopThreads                      = undef,
-                          $connector_ajp_max_threads            = undef,
-
+                          $start_stop_threads                    = undef,
+                          $notify_initscript_actions_to          = undef,
                         ) {
   Exec {
     path => '/usr/sbin:/usr/bin:/sbin:/bin',
