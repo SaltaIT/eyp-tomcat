63a80190b9d0ba44b107020fe55d54fc2cff0e39
Jordi Prats <jprats@systemadmin.es>
2017-10-16 11:16:28 +0200

    docs

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -27,6 +27,7 @@
 # nota: ssl client certificates: https://virgo47.wordpress.com/2010/08/23/tomcat-web-application-with-ssl-client-certificates/
 #
 # @param tomcatpw instance's password
+# @param catalina_base where to deploy this tomcat instance (default: /opt/<intance's name>)
 # @param connector_https_unsafe_legacy_reneg allow unsed legacy renegotation for TLS connections - HTTPS connector (default: false)
 # @param conf_dir_mode instance's config directory mode (default: 0755)
 # @param logs_dir_mode log dir mode: (default: 0755)
